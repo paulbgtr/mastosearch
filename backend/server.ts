24ceff7b6873394100bcaec78d6f98a7b69bcd88
paulbg <paulbg@hey.com>
2024-05-28 11:04:47 +0300

    chore: add errorHandler to server

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import { searchHandler } from "./handlers/searchHandler";
+import { errorHandler } from "./middleware/errorHandler";
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const port = 8080;
 app.use(express.json());
 
 app.post("/search", searchHandler);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
