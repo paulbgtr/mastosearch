ea49b6c8fd047cb77ab8efd3fa5712ab9ff5b208
paulbg <paulbg@hey.com>
2024-05-29 08:55:30 +0300

    fix: fix path of queryClient in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
-import queryClient from "./lib/QueryClient.ts";
+import queryClient from "./lib/queryClient.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
