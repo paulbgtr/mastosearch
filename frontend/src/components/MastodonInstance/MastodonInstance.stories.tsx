97398f850ff6bac60089b3473a91a03173d14e41
paulbg <paulbg@hey.com>
2024-05-29 10:45:06 +0300

    chore: use mocks in mastodon instance stories

--- frontend/src/components/MastodonInstance/MastodonInstance.stories.tsx
+++ frontend/src/components/MastodonInstance/MastodonInstance.stories.tsx
@@ -1,13 +1,8 @@
 import "src/index.css";
 import { MastodonInstance } from "./MastodonInstance";
 import { Story } from "@ladle/react";
+import { mockedMastodonInstance } from "@/data/mockedMastodonInstance";
 
 export const MastodonInstanceStory: Story = () => (
-  <MastodonInstance
-    name="Mastodon"
-    activeUsers={1000}
-    openRegistrations={true}
-    categories={["Social"]}
-    shortDescription="A social network for the fediverse"
-  />
+  <MastodonInstance {...mockedMastodonInstance} />
 );
--- frontend/src/components/MastodonInstanceList/MastodonInstanceList.stories.tsx
+++ frontend/src/components/MastodonInstanceList/MastodonInstanceList.stories.tsx
@@ -1,23 +1,7 @@
 import { Story } from "@ladle/react";
 import { MastodonInstanceList } from "./MastodonInstanceList";
-
-const instances = [
-  {
-    name: "Mastodon",
-    activeUsers: 1000,
-    openRegistrations: true,
-    categories: ["Social"],
-    shortDescription: "A social network for the fediverse",
-  },
-  {
-    name: "Pleroma",
-    activeUsers: 500,
-    openRegistrations: true,
-    categories: ["Social"],
-    shortDescription: "A lightweight social network",
-  },
-];
+import { mockedMastodonInstances } from "@/data/mockedMastodonInstances";
 
 export const MastodonInstanceListStory: Story = () => (
-  <MastodonInstanceList instances={instances} />
+  <MastodonInstanceList instances={mockedMastodonInstances} />
 );
