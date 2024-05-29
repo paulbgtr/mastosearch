0ef179e87f5e667535d29c01c1b2cabcd79068bb
paulbg <paulbg@hey.com>
2024-05-29 08:55:48 +0300

    chore: separate the fetchMastodonInstances func

---
+++ frontend/src/api/fetchMastodonInstances.ts
@@ -0,0 +1,22 @@
+/**
+ * A function that handles the main integration with the backend.
+ * It fetches the search API which provides a list of Mastodon instances based on the query.
+ *
+ * @param query
+ * @returns A list of Mastodon instances based on the query
+ */
+export const fetchMastodonInstances = async (query: string) => {
+  const res = await fetch("http://localhost:8080/search", {
+    method: "POST",
+    body: JSON.stringify({ query }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch");
+  }
+
+  return res.json();
+};
