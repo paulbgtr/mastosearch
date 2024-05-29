644d70522347779caa08d1f6ac379b7fbd7c8979
paulbg <paulbg@hey.com>
2024-05-29 08:56:07 +0300

    feat: add MastodonInstance type

---
+++ frontend/src/types/MastodonInstance.ts
@@ -0,0 +1,13 @@
+/**
+ * A type of a single Mastodon instance.
+ *
+ * Note that it doesn't have all the attributes of a Mastodon instance.
+ * For a full list of attributes, see the Mastodon Instances API documentation.
+ */
+export type MastodonInstance = {
+  name: string;
+  activeUsers: number;
+  openRegistrations: boolean;
+  categories: string[];
+  shortDescription: string;
+};
