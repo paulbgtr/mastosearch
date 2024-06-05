4ac06f4d3e61c25e51efb537c251885e1e673d84
paulbg <paulbg@hey.com>
2024-06-05 14:11:34 +0300

    chore: update MastodonInstance component
    - Display languages
    - Utilize multiple compositions pattern.
    - Use icons for open/closed registrations.
    - Add tag-like design for topics.

--- frontend/src/components/MastodonInstance/MastodonInstance.tsx
+++ frontend/src/components/MastodonInstance/MastodonInstance.tsx
@@ -8,24 +8,75 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import type { MastodonInstance as MastodonInstanceT } from "@/types/MastodonInstance";
+import { LockClosedIcon, LockOpen1Icon } from "@radix-ui/react-icons";
+
+const Category = ({ title }: { title: string }) => {
+  return <span className="px-3 py-1 bg-gray-200 rounded-xl">{title}</span>;
+};
+
+const Categories = ({ categories }: { categories: string[] }) => {
+  return (
+    <div className="flex space-x-2">
+      {categories.map((category) => (
+        <Category key={category} title={category} />
+      ))}
+    </div>
+  );
+};
+
+const RegistrationStatus = ({
+  openRegistrations,
+}: {
+  openRegistrations: boolean;
+}) => {
+  return (
+    <p>
+      {openRegistrations ? (
+        <>
+          <LockOpen1Icon className="inline-block w-4 h-4" /> Open registrations
+        </>
+      ) : (
+        <>
+          <LockClosedIcon className="inline-block w-4 h-4" /> Closed
+          registrations
+        </>
+      )}
+    </p>
+  );
+};
+
+const trimDescription = (description: string) => {
+  if (!description) return "No description";
+
+  if (description.length > 100) {
+    return description.substring(0, 100) + "...";
+  }
+  return description;
+};
 
 export const MastodonInstance = ({
   name,
+  languages,
   activeUsers,
   openRegistrations,
   categories,
   shortDescription,
 }: MastodonInstanceT) => {
+  const trimmedDescription = trimDescription(shortDescription);
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>{name}</CardTitle>
-        <CardDescription>{shortDescription}</CardDescription>
+        <CardDescription className="overflow-hidden text-ellipsis whitespace-nowrap">
+          {trimmedDescription}
+        </CardDescription>
       </CardHeader>
-      <CardContent>
+      <CardContent className="space-y-1 ">
         <p>Active users: {activeUsers}</p>
-        <p>Open registrations: {openRegistrations ? "Yes" : "No"}</p>
-        <p>Categories: {categories.join(", ")}</p>
+        <RegistrationStatus openRegistrations={openRegistrations} />
+        {languages && <p>Languages: {languages.join(", ")}</p>}
+        <Categories categories={categories} />
       </CardContent>
       <CardFooter className="flex justify-between">
         <a
