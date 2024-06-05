7025e09217f2ab748bdae8271feef984efbc3d88
paulbg <paulbg@hey.com>
2024-06-05 12:54:05 +0300

    chore: implement context in SearchInput

--- frontend/src/components/Search/SearchBar/SearchInput.tsx
+++ frontend/src/components/Search/SearchBar/SearchInput.tsx
@@ -1,26 +1,24 @@
 import { Input } from "@/components/ui/input";
+import { useContext } from "react";
+import { SearchContext } from "../../context/SearchContext";
 
 /**
  * A search input component that takes in a query, a function to set the query, a ref to the input element, and a search example
  *
- * @param query - the current query
- * @param setQuery - a function to set the query
  * @param inputRef - a ref to the input element
  * @param searchExample - an example of what to search for
  *
  * @returns a search input component
  */
 export const SearchInput = ({
-  query,
-  setQuery,
   inputRef,
   searchExample,
 }: {
-  query: string;
-  setQuery: (query: string) => void;
   inputRef: React.RefObject<HTMLInputElement>;
   searchExample: string;
 }) => {
+  const { query, setQuery } = useContext(SearchContext);
+
   return (
     <Input
       value={query}
