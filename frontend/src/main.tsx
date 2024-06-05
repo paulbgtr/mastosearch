dfde2bed431ea7c19aa8387c2841cfc586bc02c5
paulbg <paulbg@hey.com>
2024-06-05 12:51:55 +0300

    chore: wrap main with a SearchProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "./lib/queryClient.ts";
+import { SearchProvider } from "./components/context/SearchContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <SearchProvider>
+        <App />
+      </SearchProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
