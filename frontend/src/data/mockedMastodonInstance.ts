83d33919294451f97c088cbe65b7b55e7e55c71a
paulbg <paulbg@hey.com>
2024-06-07 11:05:26 +0300

    fix: add languages prop to mockedMastodonInstance

--- frontend/src/data/mockedMastodonInstance.ts
+++ frontend/src/data/mockedMastodonInstance.ts
@@ -1,11 +1,14 @@
+import type { MastodonInstance } from "@/types/MastodonInstance";
+
 /**
  * A mocked Mastodon instance object.
  *
  * Is used for testing the MastodonInstance component.
  */
-export const mockedMastodonInstance = {
+export const mockedMastodonInstance: MastodonInstance = {
   name: "Mastodon",
   activeUsers: 1000,
+  languages: ["en"],
   openRegistrations: true,
   categories: ["Social"],
   shortDescription: "A social network for the fediverse",
